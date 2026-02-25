0ffbd02859ec3460ea8152a9dfc12e2c0116ab13
Ivica Drusany <icom4@MacBook-Pro-M4.local>
2026-02-25 16:27:01 +0100

    Deploy: 2026-02-25 16:27

--- _next/static/4OZJ2bSBRt9llSCxLTd4O/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fadmin\u002Fblog\u002Fedit\u002F[id]","\u002Fblog\u002F[[...segments]]","\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
